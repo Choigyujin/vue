fa6b9a20f79ba36658c22454e47e417ea6bffed8
Choigyujin <rbwls111822@gmail.com>
2021-03-16 19:27:55 +0900

    ggyu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <Todolist></Todolist>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/Todoinput.vue'
+import Todolist from './components/Todolist.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  component:{
+   'TodoHeader':TodoHeader,
+   'TodoInput':TodoInput,
+   'Todolist':Todolist,
+   'TodoFooter':TodoFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
